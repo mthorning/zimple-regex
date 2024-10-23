1bc41c61c6a3d7cf537fc4e64dd3f7c02e79da7a
Matt Thorning <matthewthorning@gmail.com>
2024-10-23 19:49:34 +0100

    tagged unions up to the eyeballs

--- src/tokenizer.zig
+++ src/tokenizer.zig
@@ -1,18 +1,36 @@
 const std = @import("std");
 const testing = std.testing;
 
-const TokenType = enum {
+// Scalars
+const SType = enum {
+    literal,
     start_of_line,
     end_of_line,
-    literal,
     any,
 };
 
-const Token = union(TokenType) {
+const SToken = union(SType) {
+    literal: u8,
     start_of_line,
     end_of_line,
-    literal: u8,
     any,
+};
+
+// Quantifiers
+const QType = enum { one_or_more };
+
+const QToken = union(QType) {
+    one_or_more: SType,
+};
+
+const TokenType = enum {
+    scalar,
+    quantifier,
+};
+
+const Token = union(TokenType) {
+    scalar: SToken,
+    quantifier: QToken,
 };
 
 fn tokenize(allocator: std.mem.Allocator, re: []const u8) ![]Token {
@@ -22,13 +40,18 @@ fn tokenize(allocator: std.mem.Allocator, re: []const u8) ![]Token {
     var tokens_cursor: usize = 0;
 
     while (re_cursor < re.len) {
-        tokens[tokens_cursor] = switch (re[re_cursor]) {
-            '^' => Token.start_of_line,
-            '$' => Token.end_of_line,
-            '.' => Token.any,
-            else => Token{ .literal = re[re_cursor] },
+        const new_token = switch (re[re_cursor]) {
+            '^' => .{ .scalar = .start_of_line },
+            '$' => .{ .scalar = .end_of_line },
+            '.' => .{ .scalar = .any },
+            '+' => blk: {
+                const one_or_more_value = tokens[tokens_cursor];
+                break :blk Token{ .quantifier = .{ .one_or_more = one_or_more_value } };
+            },
+            else => .{ .scalar = .{ .literal = re[re_cursor] } },
         };
 
+        tokens[tokens_cursor] = new_token;
         re_cursor += 1;
         tokens_cursor += 1;
     }
@@ -36,16 +59,26 @@ fn tokenize(allocator: std.mem.Allocator, re: []const u8) ![]Token {
     return tokens;
 }
 
-test tokenize {
+test "basics" {
     const allocator = testing.allocator;
     const tokens = try tokenize(allocator, "^h.i$");
     defer allocator.free(tokens);
 
     try testing.expectEqualSlices(Token, tokens, &[_]Token{
-        .start_of_line,
-        .{ .literal = 'h' },
-        .any,
-        .{ .literal = 'i' },
-        .end_of_line,
+        .{ .scalar = .start_of_line },
+        .{ .scalar = .{ .literal = 'h' } },
+        .{ .scalar = .any },
+        .{ .scalar = .{ .literal = 'i' } },
+        .{ .scalar = .end_of_line },
     });
 }
+
+// test "quantifiers" {
+//     const allocator = testing.allocator;
+//     const tokens = try tokenize(allocator, "h+");
+//     defer allocator.free(tokens);
+
+//     try testing.expectEqualSlices(Token, tokens, &[_]Token{
+//         .{ .one_or_more = .{ .literal = 'h' } },
+//     });
+// }
